c35abe7ace479692255997be518439bd3b9b7fae
minsik <a184562@gmail.com>
2023-07-16 21:56:33 +0900

    Unity Toy Project(2D Platformer) 6th Day

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -83,4 +83,37 @@ public class PlayerMove : MonoBehaviour
             }
         }
      }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            OnDamaged(collision.transform.position);
+        }
+    }
+
+    void OnDamaged(Vector2 targetPos)
+    {
+        // Change Layer (Immortal Active)
+        gameObject.layer = 11;
+
+        // View Alpha
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        // Reaction Force
+        int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(dirc, 1),  ForceMode2D.Impulse);
+
+        // Animation
+        anim.SetTrigger("isDamage");
+
+
+        Invoke("OffDamaged", 2);
+    }
+
+    void OffDamaged()
+    {
+        gameObject.layer = 10;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
+    }
 }
